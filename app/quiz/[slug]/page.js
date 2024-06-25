35c87d1e0219d6f66fbd563e2bffea34d76d233d
hitochix06 <pachara.philibert@hotmail.fr>
2024-06-25 22:16:56 +0200

    chore: Mettre à jour les styles CSS et les traductions de la page du classement

--- app/quiz/[slug]/page.js
+++ app/quiz/[slug]/page.js
@@ -59,15 +59,14 @@ export default function Page({ params }) {
   };
 
   return (
-    <div className="grid grid-cols-12 grow">
+    <div className="">
       {/* Section gauche avec l'image de la catégorie */}
-      <div
-        className="col-span-12 lg:col-span-4 bg-center bg-cover bg-no-repeat bg-black min-h-8"
-        style={{ backgroundImage: `url('/illustrations/${params.slug}.webp')` }}
-      ></div>
 
       {/* Section droite avec les questions et les réponses */}
-      <div className="col-span-12 lg:col-span-8 p-4 lg:px-24 flex flex-col justify-between">
+      <div
+        className="col-span-12 lg:col-span-8 p-4 lg:px-24 flex flex-col justify-between bg-cover bg-center"
+        style={{ backgroundImage: `url('/illustrations/${params.slug}.webp')` }}
+      >
         {questions.length && currentIndex !== questions.length ? (
           <>
             <div>
@@ -78,13 +77,15 @@ export default function Page({ params }) {
                   {questions.length}
                 </span>
               </div>
-              <div className="text-center">
+
+              <div className="text-center ">
                 {/* Affichage de la catégorie */}
-                <div className="text-blue-500 animate-pulse-slow text-4xl font-bold capitalize">
+                <div className="text-blue-500 animate-pulse-slow text-4xl font-bold capitalize ">
                   {slugToNameCategory(params.slug)}
                 </div>
+
                 {/* Affichage de la question actuelle */}
-                <div className="lg:min-h-28 mt-3 grid">
+                <div className="lg:min-h-28 mt-3 grid bg-white p-4 rounded-lg  gap-6">
                   <AnimatePresence mode="wait">
                     <motion.h1
                       initial={{ opacity: 0, y: -20 }}
@@ -96,34 +97,35 @@ export default function Page({ params }) {
                         damping: 20,
                       }}
                       key={currentIndex}
-                      className="text-center lg:text-3xl mt-2 lg:mt-8"
+                      className="text-center lg:text-3xl mt-2 lg:mt-8 "
                     >
                       {questions[currentIndex].question}
                     </motion.h1>
                   </AnimatePresence>
+
+                  {/* Affichage des réponses */}
+                  <div className="flex flex-wrap justify-center gap-3">
+                    <Answers
+                      id={questions[currentIndex]._id}
+                      answers={questions[currentIndex].allAnswers}
+                      selectedAnswer={selectedAnswer}
+                      selectAnswer={selectAnswer}
+                    />
+                  </div>
+                  {/* Bouton pour valider la réponse */}
+                  <div className="text-center my-8 ">
+                    {" "}
+                    {/* Modification de "text-end" à "text-center" pour centrer le bouton */}
+                    <div
+                      onClick={validateAnswer}
+                      className={`btn px-8 text-lg text-white ${
+                        selectedAnswer ? "bouton-primary" : "btn-disabled"
+                      }`}
+                    >
+                      Valider
+                    </div>
+                  </div>
                 </div>
-              </div>
-
-              {/* Affichage des réponses */}
-              <div className="flex mt-8 gap-6 min-h-32 m-auto content-center flex-wrap justify-center max-w-3xl ">
-                <Answers
-                  id={questions[currentIndex]._id}
-                  answers={questions[currentIndex].allAnswers}
-                  selectedAnswer={selectedAnswer}
-                  selectAnswer={selectAnswer}
-                />
-              </div>
-            </div>
-
-            {/* Bouton pour valider la réponse */}
-            <div className="text-end my-8 ">
-              <div
-                onClick={validateAnswer}
-                className={`btn px-8 text-lg text-white ${
-                  selectedAnswer ? "bouton-primary" : "btn-disabled"
-                }`}
-              >
-                Valider
               </div>
             </div>
           </>
